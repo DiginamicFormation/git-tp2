94ac688a430ad1897f75dec6bec21c5ae355006e
Odrey142 <106230623+Odrey142@users.noreply.github.com>
2022-10-05 17:09:50 +0200

    Update ClassB.java

--- src/org/diginamic/ClassB.java
+++ src/org/diginamic/ClassB.java
@@ -1,8 +1,23 @@
 package org.diginamic;
+<<<<<<< Updated upstream
 
 public class ClassB {
 	
 	public void nvMethod1() {
 		System.out.println("Je suis la mauvaise méthode.");
+=======
+<<<<<<< HEAD
+public class ClassB {
+	
+	public void nvMethod1() {
+		System.out.println("Je suis la bonne méthode.");
+=======
+
+public class ClassB {
+	
+	public void nvMethod1() {
+		System.out.println("Je suis la mauvaise méthode.");
+>>>>>>> durand
+>>>>>>> Stashed changes
 	}
 }
